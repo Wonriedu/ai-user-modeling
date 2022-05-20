82e62281f29bf146332197b30ff74a80049276ee
hcnoh <rhc0624@gmail.com>
2022-05-20 10:41:57 +0900

    model init

---
+++ models/_20220520_00.py
@@ -0,0 +1,14 @@
+import torch
+
+from torch.nn import Module
+
+
+class UserModel(Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+    def train(self):
+        pass
