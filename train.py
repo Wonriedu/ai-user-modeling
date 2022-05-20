74ab7e541ecd1d67eb6c06dea84cc849f0df1a98
hcnoh <rhc0624@gmail.com>
2022-05-20 13:14:24 +0900

    train.py 작성 진행

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader, random_split
 from torch.optim import Adam
 
 from data_loaders.pulja_data_loader import PuljaDataLoader
+from models._20220520_00 import UserModel
 from models.utils import collate_fn
 
 
@@ -17,8 +18,11 @@ def main():
         os.mkdir(ckpt_path)
 
     batch_size = 256
-    seq_len = 100
     train_ratio = 0.9
+
+    seq_len = 100
+
+    dim_v = 50
 
     dataset = PuljaDataLoader(seq_len)
 
@@ -26,3 +30,27 @@ def main():
         device = "cuda"
     else:
         device = "cpu"
+
+    model = UserModel(dataset.num_c, dataset.num_d, dim_v).to(device)
+
+    train_size = int(len(dataset) * train_ratio)
+    test_size = len(dataset) - train_size
+
+    train_dataset, test_dataset = random_split(
+        dataset, [train_size, test_size]
+    )
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True,
+        collate_fn=collate_fn
+    )
+    test_loader = DataLoader(
+        test_dataset, batch_size=test_size, shuffle=True,
+        collate_fn=collate_fn
+    )
+
+    opt = Adam(model.parameters())
+
+
+if __name__ == "__main__":
+    main()
