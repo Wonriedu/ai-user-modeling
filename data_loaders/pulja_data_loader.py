2f4636bf5db7ed5c6f6073c530c8d1d979639a6c
hcnoh <rhc0624@gmail.com>
2022-06-13 10:36:53 +0900

    category1 포함 전처리 구현

--- data_loaders/pulja_data_loader.py
+++ data_loaders/pulja_data_loader.py
@@ -39,37 +39,6 @@ class PuljaDataLoader(Dataset):
             os.path.join(self.table_dir, "tb_curriculum_unit.csv")
         )
 
-        # 3등급 커리큘럼 예상 풀이시간 기준으로 작성
-        # self.difficulty2duration = {
-        #     1.0: 0.75,
-        #     2.0: 0.75,
-        #     3.0: 1.0,
-        #     4.0: 1.5,
-        #     5.0: 2.5,
-        #     6.0: 7,
-        #     7.0: 10,
-        # }
-        # 임의 기준 01
-        # self.difficulty2duration = {
-        #     1.0: 0.75,
-        #     2.0: 0.75,
-        #     3.0: 1.0,
-        #     4.0: 1.0,
-        #     5.0: 1.0,
-        #     6.0: 1.5,
-        #     7.0: 1.5,
-        # }
-        # 임의 기준 02
-        # self.difficulty2duration = {
-        #     1.0: 1.5,
-        #     2.0: 2.0,
-        #     3.0: 2.5,
-        #     4.0: 2.5,
-        #     5.0: 2.5,
-        #     6.0: 3.0,
-        #     7.0: 7.0,
-        # }
-        # 임의 기준 03
         self.difficulty2duration = {
             1.0: 1.5,
             2.0: 2.0,
@@ -87,7 +56,9 @@ class PuljaDataLoader(Dataset):
             .merge(self.tb_problems)\
             .sort_values("seq")\
             .reset_index(drop=True)
-        self.df = self.df[self.df.notnull()["category3"]]\
+        self.df = self.df[self.df.notnull()["category1"]]\
+            .reset_index(drop=True)
+        self.df = self.df[self.df.notnull()["category2"]]\
             .reset_index(drop=True)
 
         self.u_list = np.unique(self.df["userSeq"].values)
@@ -95,8 +66,11 @@ class PuljaDataLoader(Dataset):
 
         self.num_u = self.u_list.shape[0]
 
-        self.c_list = np.unique(self.df["category2"].values)
-        self.c2idx = {c: i for i, c in enumerate(self.c_list)}
+        self.c1_list = np.unique(self.df["category1"].values)
+        self.c12idx = {c1: i for i, c1 in enumerate(self.c1_list)}
+
+        self.c2_list = np.unique(self.df["category2"].values)
+        self.c22idx = {c2: i for i, c2 in enumerate(self.c2_list)}
 
         self.num_c = self.c_list.shape[0]
 
@@ -111,33 +85,41 @@ class PuljaDataLoader(Dataset):
             with open(
                 os.path.join(self.dataset_dir, "dataset.pkl"), "rb"
             ) as f:
-                self.c_seqs, self.d_seqs, self.r_seqs = pickle.load(f)
+                self.c1_seqs, self.c2_seqs, self.d_seqs, self.r_seqs = \
+                    pickle.load(f)
         else:
             self.preprocess()
 
             with open(
                 os.path.join(self.dataset_dir, "dataset.pkl"), "wb"
             ) as f:
-                pickle.dump((self.c_seqs, self.d_seqs, self.r_seqs), f)
+                pickle.dump(
+                    (self.c1_seqs, self.c2_seqs, self.d_seqs, self.r_seqs), f
+                )
 
         self.len = len(self.r_seqs)
 
     def __getitem__(self, idx):
-        return self.c_seqs[idx], self.d_seqs[idx], self.r_seqs[idx]
+        return self.c1_seqs[idx], self.c2_seqs[idx], self.d_seqs[idx], \
+            self.r_seqs[idx]
 
     def __len__(self):
         return self.len
 
     def preprocess(self):
-        self.c_seqs = []
+        self.c1_seqs = []
+        self.c2_seqs = []
         self.d_seqs = []
         self.r_seqs = []
 
         for u in self.u_list:
             df_u = self.df[self.df["userSeq"] == u]
 
-            c_seq = np.array(
-                [self.c2idx[c] for c in df_u["category2"].values]
+            c1_seq = np.array(
+                [self.c12idx[c1] for c1 in df_u["category1"].values]
+            )
+            c2_seq = np.array(
+                [self.c22idx[c2] for c2 in df_u["category2"].values]
             )
             d_seq = np.array(
                 [self.d2idx[d] for d in df_u["difficulty_cd"].values]
@@ -152,45 +134,20 @@ class PuljaDataLoader(Dataset):
             r_seq = (df_u["isCorrect"].values == "Y").astype(float)
             r_seq = self.get_response(r_seq, TR_seq)
 
-            self.c_seqs.append(c_seq)
+            self.c1_seqs.append(c1_seq)
+            self.c2_seqs.append(c2_seq)
             self.d_seqs.append(d_seq)
             self.r_seqs.append(r_seq)
 
         if self.seq_len:
-            self.c_seqs, self.d_seqs, self.r_seqs = match_seq_len(
-                self.c_seqs, self.d_seqs, self.r_seqs, self.seq_len
-            )
+            self.c1_seqs, self.c2_seqs, self.d_seqs, self.r_seqs = \
+                match_seq_len(
+                    self.c1_seqs,
+                    self.c2_seqs,
+                    self.d_seqs,
+                    self.r_seqs,
+                    self.seq_len
+                )
 
     def get_response(self, r_seq, TR_seq):
         return r_seq * (TR_seq <= 1.0).astype(float)
-
-    # def get_response(self, r_seq, TR_seq):
-    #     result = []
-    #     for r, TR in zip(r_seq, TR_seq):
-    #         if r == 1:
-    #             basis = 0.5
-    #             TR = np.minimum(TR, 1.5)
-
-    #             result.append(sigmoid(
-    #                 basis *
-    #                 (np.tan((np.pi / 2) - (TR * np.pi / 3)))
-    #             ))
-    #         else:
-    #             basis = -0.5
-    #             TR = np.maximum(TR, 0.65)
-
-    #             result.append(sigmoid(
-    #                 basis *
-    #                 (np.tan((np.pi / 2) - ((1 / TR) * np.pi * 0.65 / 2)))
-    #             ))
-
-    #     return np.array(result)
-
-    # def get_response(self, r, TR):
-    #     return r * \
-    #         (
-    #             1.50 - np.minimum(
-    #                 np.maximum(TR, 0.65), 1.50
-    #             )
-    #         ) / \
-    #         (1.50 - 0.65)
