717372c32aa0380138301c0721973c85d96d9c03
hcnoh <rhc0624@gmail.com>
2022-07-27 15:02:08 +0900

    _20220727_00 구현

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader, random_split
 from torch.optim import Adam
 
 from data_loaders.pulja_data_loader_02 import PuljaDataLoader
-from models._20220726_20 import UserModel
+from models._20220727_00 import UserModel
 from models.utils_02 import collate_fn
 
 
